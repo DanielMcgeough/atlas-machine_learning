0afcd9411873873dabe82b639b3f72d77a9e79a4
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-08-29 14:01:00 -0500

    Setup main and groundwork.

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" module for element wise ops"""
+
+
+def add_arrays(arr1, arr2):
+    """adding arrays element wise
+        elements are ints or floats
+        for now
+    """
+    new_array = []
