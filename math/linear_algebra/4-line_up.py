ab10d3b909bce68fe9c115888c69c2c28dd18204
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-08-29 14:05:34 -0500

    spelling mistakes.

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -10,6 +10,6 @@ def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         return None
     result = []
-    for i in range (len(arr1)):
-        result.append(arr1[1] + arr2[i])
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
     return result
