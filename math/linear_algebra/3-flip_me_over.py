141ec90e583e96b7043f861b63ca9a1018b48728
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-08-29 13:55:33 -0500

    I dont like nested for loops

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,3 +5,9 @@
 def matrix_transpose(matrix):
     """transposing a 2d Matrix by 'rotating' it"""
     transposed_matrix = []
+    for i in range(len(matrix[0])):
+        row = []
+        for j in range(len(matrix)):
+            row.append(matrix[j][i])
+        transposed_matrix.append(row)
+    return transposed_matrix
