cfc8f3b18687a178f96ddf12d3543cc593780c8d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-01 11:11:49 -0500

    only a few more, numpy is great.

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module defines the np_transpose method for transposing matrices """
+
+
+def np_transpose(matrix):
+    """ Returns the transpose of a numpy.ndarray
+        I want to call it numpty
+    """
+    return matrix.transpose()
