4c514b4bb9cd74d0937443051d7e5f0fc8878e7b
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-01 10:56:37 -0500

    modifications broke it.

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,15 +4,17 @@
 
 def mat_mul(mat1, mat2):
     """multiplies two matrices containing ints or floats"""
-    if len(mat1[0]) !=(mat2):
+    if len(mat1[0]) != len(mat2):
         return None
-    rows1, columns1 = len(mat1), len(mat1[0])
-    rows2, columns2 = len(mat2), len(mat2[0])
 
-    result = [[0 for _ in range(columns2)] for _ in range(rows1)]
+    rows1, cols1 = len(mat1), len(mat1[0])
+    rows2, cols2 = len(mat2), len(mat2[0])
+
+    result = [[0 for _ in range(cols2)] for _ in range(rows1)]
 
     for i in range(rows1):
-        for j in range(columns2):
-            for k in range(columns1):
+        for j in range(cols2):
+            for k in range(cols1):
                 result[i][j] += mat1[i][k] * mat2[k][j]
+
     return result
