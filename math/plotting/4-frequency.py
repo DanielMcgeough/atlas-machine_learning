97dbfc13bafba409e2824944e9a7a4fd2b7c8128
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-02 16:10:37 -0500

    changed it back.

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-"""Plotting a histogram"""
+"""Histogram stuff """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Func to plot a histogram of student scores"""
+    """Histogram function"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    plt.hist(student_grades,
-             bins=range(1, 101, 10),
-             edgecolor='black', linewidth=1)
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black',
+             linewidth=1)
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.xlim(0, 100)
