18f6c985c81e0ed5660a403bee0d2c31c9b7421b
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-02 16:07:31 -0500

    Frequency done.

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Plotting a histogram"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Func to plot a histogram of student scores"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades,
+             bins=range(1, 101, 10),
+             edgecolor='black', linewidth=1)
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.title('Project A')
+    plt.xticks(range(0, 101, 10))
+
+    plt.show()
