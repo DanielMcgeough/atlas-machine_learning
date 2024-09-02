ac8c89f8c81bbcb6d97912d7db4f91e0849abaff
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-02 15:25:19 -0500

    new dir and creating 0 task

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This is the module to create a line plot using Matplotlib"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """Creates a line plot"""
+    x = np.arrange(0, 11)
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y, 'r-')
+    
---
+++ math/plotting/README.md
@@ -0,0 +1,3 @@
+This is the README for plotting. Before now most of the data we dealt with was arranged in
+a manner that would be shown by numbers and not in a graphic way. Plotting relies on writing
+code that can represent data sets in a graphically distinct way.
