2fa9b58ac770c18a8971b08f60d3ad4dfcde993e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-11 11:04:06 -0500

    establishment of expo stuff, similar to Poisson

---
+++ math/probability/exponential.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Exponential Distribution
+Exponential- The exponential distribution is a continuous
+probability distribution that describes the time between
+events in a Poisson process. It is often used to model the
+time between occurrences of random events, such as the time
+between arrivals at a service station or the time until a
+machine failure.
+"""
+
+
+class Exponential:
+    """creates a class for Expo Dist"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes exponential distribution"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(1 / (sum(data) / len(data)))
