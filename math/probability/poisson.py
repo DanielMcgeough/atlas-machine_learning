f0c3038bb41d2dfd99ecadf5e9988cf40479a652
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-11 10:30:48 -0500

    running the pmf.

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -26,3 +26,22 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Create a PMF instance which calculates successes"""
+        k = int(k)
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+        e_lambda = e ** (-self.lambtha)
+
+        lambda_k = self.lambtha ** k
+
+        k_factorial = 1
+        for i in range(1, k + 1):
+            k_factorial *= i
+        
+        return (e_lambda * lambda_k) / k_factorial
+        """base of natural logarithm multiplied by
+        the average rate of events equals k factorial"""
