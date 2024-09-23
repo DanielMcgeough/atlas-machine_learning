bdcd31a3216793301bb3ea948f1ad5345b46735d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-23 16:12:11 -0500

    main and task 2 files setup.

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module documentation for py files"""
+import numpy as np
+
+
+class Neuron:
+    """A class that defines a neuron"""
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calcualtes forward propagation of neuron"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
