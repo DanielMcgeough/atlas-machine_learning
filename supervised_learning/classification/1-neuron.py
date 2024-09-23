404f263923a621d89d42dbc285240db598c56934
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-23 16:08:58 -0500

    rework.

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -6,15 +6,13 @@ import numpy as np
 class Neuron:
     """A class that defines a neuron"""
     def __init__(self, nx):
-        """Initializes neuron class"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
