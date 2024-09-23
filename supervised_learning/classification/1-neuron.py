47acc1ea3603ddca79297d84aef077c71cc419f4
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-23 16:04:36 -0500

    added properties for initialized values to make a neuron.

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module documentation for py files"""
+import numpy as np
+
+
+class Neuron:
+    """A class that defines a neuron"""
+    def __init__(self, nx):
+        """Initializes neuron class"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
+
+    @property
+    def W(self):
+        return self.__W
+    @property
+    def b(self):
+        return self.__b
+    @property
+    def A(self):
+        return self.A
