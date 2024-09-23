989e992305b5f62f19a3cb837e9620167c516d01
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-23 17:29:44 -0500

    rework.

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """class for NeuralNetwork setup"""
+    """class for NeuralNetwork"""
     def __init__(self, nx, nodes):
-        """constructor for NeuralNetwork to initialise"""
+        """constructor for NeuralNetwork"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -48,7 +48,7 @@ class NeuralNetwork:
         return self.__A2
 
     def forward_prop(self, X):
-        """Enables forward propagation"""
+        """method for forward propagation"""
         Z1 = np.dot(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-Z1))
         Z2 = np.dot(self.__W2, self.__A1) + self.__b2
@@ -58,20 +58,20 @@ class NeuralNetwork:
     def cost(self, Y, A):
         """calculates the cost of the model using logistic regression"""
         m = Y.shape[1]
-        cost = -1 / m * np.sum(
+        cost = -(1 / m) * np.sum(
             Y * np.log(A) + (1 - Y) *
             np.log(1.0000001 - A))
         return cost
 
     def evaluate(self, X, Y):
-        """How Right is the prediction"""
-        A1, A2 = self.forward_prop(X)
-        cost = self.cost(Y, A2)
-        prediction = np.where(A2 >= 0.5, 1, 0)
+        """defines a neutal network with one hidden layer"""
+        self.forward_prop(X)
+        cost = self.cost(Y, self.__A2)
+        prediction = np.where(self.__A2 >= 0.5, 1, 0)
         return prediction, cost
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
-        """defines a neural network with one hidden layer"""
+        """defines a neural network with one hidden layer """
         m = X.shape[1]
 
         dZ2 = A2 - Y
