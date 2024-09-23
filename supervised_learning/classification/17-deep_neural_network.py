834514ff65ea82230e95ea53bd9849e9dcf02a57
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-09-23 18:15:24 -0500

    testing spacing.

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -38,9 +38,11 @@ class DeepNeuralNetwork:
     @property
     def L(self):
         return self.__L
+
     @property
     def cache(self):
         return self.__cache
+
     @property
     def weights(self):
         return self.__weights
