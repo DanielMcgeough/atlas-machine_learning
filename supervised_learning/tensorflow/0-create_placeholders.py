577a3d7a321c910dfe4e9d21efb6f6d88a9c7119
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-01 18:00:14 -0500

    pycodestyle st00f

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -7,7 +7,7 @@ def create_placeholders(nx, classes):
     """returns placeholders named x and y for
     input data to the neural network and
     one-hot labels for input data respectivelys"""
-    x = tf.placeholder(tf.float32, shape=[None, nx], name ='x')
-    y = tf.placeholder(tf.float32, shape=[None, classes], name ='y')
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
 
     return x, y
