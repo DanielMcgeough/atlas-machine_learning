47ef57c1c298ec7cee6f5b62672aa20a5e1db2ab
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-01 17:59:06 -0500

    added dox and name params

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""for tensorflow without Keras"""
 import tensorflow.compat.v1 as tf
 
 
@@ -6,7 +7,7 @@ def create_placeholders(nx, classes):
     """returns placeholders named x and y for
     input data to the neural network and
     one-hot labels for input data respectivelys"""
-    x = tf.placeholder(tf.float32, shape=[None, nx])
-    y = tf.placeholder(tf.float32, shape=[None, classes])
+    x = tf.placeholder(tf.float32, shape=[None, nx], name ='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name ='y')
 
     return x, y
