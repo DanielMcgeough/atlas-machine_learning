92ac6b2173ccfe6b99facb2a610f2346ec14f263
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-01 18:08:44 -0500

    they love cranking the difficulty don't they.

---
+++ supervised_learning/tensorflow/1-create-layer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""for tensorflow without Keras"""
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """function to create layers in neural network"""
+    v = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    layer = tf.keras.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=v,
+        name='layer'
+    )
+    return layer(prev)
