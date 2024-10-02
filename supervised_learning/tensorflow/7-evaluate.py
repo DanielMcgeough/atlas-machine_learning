9cd0fb7a4cd03f4a2429fb7660fdffeea6d6d161
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-02 15:26:05 -0500

    trying it.

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+def evaluate(X, Y, save_path):
+  """Evaluates the output of a neural network.
+
+  Args:
+    X: A numpy.ndarray containing the input data to evaluate.
+    Y: A numpy.ndarray containing the one-hot labels for X.
+    save_path: The location to load the model from.
+
+  Returns:
+    The network's prediction, accuracy, and loss, respectively.
+  """
+
+  # Load the saved model
+  saver = tf.train.import_meta_graph(save_path + '.meta')
+
+  with tf.Session() as sess:
+    # Restore the saved variables
+    saver.restore(sess, save_path)
+
+    # Get the placeholders, tensors, and operations from the graph's collection
+    x = tf.get_collection('placeholders')[0]
+    y = tf.get_collection('placeholders')[1]
+    y_pred = tf.get_collection('tensors')[0]
+    loss = tf.get_collection('tensors')[1]
+    accuracy = tf.get_collection('tensors')[2]
+
+    # Evaluate the model
+    prediction, loss_value, accuracy_value = sess.run([y_pred, loss, accuracy], feed_dict={x: X, y: Y})
+
+  return prediction, accuracy_value, loss_value
