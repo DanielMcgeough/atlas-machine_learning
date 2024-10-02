35dc6cd99e53188253e785145317bd77b862caba
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-01 23:59:45 -0500

    handling setting up the early stuff

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""for tensorflow without Keras... explicitly"""
+import tensorflow.compat.v1 as tf
+
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid,
+          layer_sizes, activations, alpha,
+          iterations, save_path='/tmp/model.ckpt'):
+    """This exhaustively
+    long task will build,
+    train and save a neural
+    network classifier"""
+    x, y = create_placeholders(layer_sizes[0], layer_sizes[-1])
+    """This creates placeholder for input and labels"""
+    y_pred = forward_prop(x, layer_sizes, activations)
+    """handles forward propagation"""
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+    """handles calculations for loss, accuracy
+    and training setup"""
+    
