711e05de0c2e08cee7f4e0d41981cd7849734ac0
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-02 15:30:58 -0500

    modified some stuff.

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -11,37 +11,25 @@ forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
   """
-  Builds, trains, and saves a neural network classifier.
-
-  Args:
-    X_train: A numpy.ndarray containing the training input data.
-    Y_train: A numpy.ndarray containing the training labels.
-    X_valid: A numpy.ndarray containing the validation input data.
-    Y_valid: A numpy.ndarray containing the validation labels.
-    layer_sizes: A list containing the number of nodes in each layer of the network.
-    activations: A list containing the activation functions for each layer of the network.
-    alpha: The learning rate.
-    iterations: The number of iterations to train over.
-    save_path: The path where the model should be saved.
-
-  Returns:
-    The path where the model was saved.
+  Builds, trains,
+  and saves a neural network classifier.
   """
 
-  # Create placeholders for input data and labels
+  """Create placeholders for input data and labels"""
   x, y = create_placeholders(layer_sizes[0], layer_sizes[-1])
 
-  # Forward propagation
+  """Forward propagation"""
   y_pred = forward_prop(x, layer_sizes, activations)
 
-  # Calculate loss and accuracy
+  """Calculate loss and accuracy"""
   loss = calculate_loss(y, y_pred)
   accuracy = calculate_accuracy(y, y_pred)
 
-  # Create training operation
+  """Create training operation"""
   train_op = create_train_op(loss, alpha)
 
-  # Add placeholders, tensors, and operation to the graph's collection
+  """Add placeholders, tensors, and operation
+  to the graph's collection"""
   tf.add_to_collection('placeholders', x)
   tf.add_to_collection('placeholders', y)
   tf.add_to_collection('tensors', y_pred)
@@ -49,21 +37,19 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
   tf.add_to_collection('tensors', accuracy)
   tf.add_to_collection('operations', train_op)
 
-  # Create a saver
+  """Create a saver"""
   saver = tf.train.Saver()
 
   with tf.Session() as sess:
-    # Initialize variables
+    """initialize variables"""
     sess.run(tf.global_variables_initializer())
 
-    # Train the model
+    """Train the model with a for loop"""
     for i in range(iterations):
-      # Train the model
       _, train_cost, train_acc = sess.run([train_op, loss, accuracy], feed_dict={x: X_train, y: Y_train})
 
       # ... (rest of your training loop here)
 
-  # Save the model
   saver.save(sess, save_path)
 
   return save_path
