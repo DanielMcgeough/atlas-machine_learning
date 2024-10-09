396e426110f68fb66d18a6073abfbc14c7aeec71
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-09 16:20:40 -0500

    woo mix it up.

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Stupid stuff making me"""
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """Updates a variable using the gradient descent with momentum optimization algorithm.   
+
+
+    Args:
+        alpha: The learning rate.
+        beta1: The momentum weight.
+        var: A numpy.ndarray containing the variable to be updated.
+        grad: A numpy.ndarray containing the gradient of var.
+        v: The previous first moment of var.
+
+    Returns:
+        The updated variable and the new moment, respectively.
+    """
+
+    # Update the first moment
+    v = beta1 * v + (1 - beta1) * grad
+
+    # Update the variable
+    var = var - alpha * v
+
+    return var, v
