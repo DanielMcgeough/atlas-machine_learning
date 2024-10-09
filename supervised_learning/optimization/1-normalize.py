3793c016d6173bb70a50639ea6730c0f53208ed0
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-09 15:56:07 -0500

    try this on for size.

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This is needed to pass checker"""
+import numpy as np
+
+
+def normalize(X, m, s):
+  """Normalizes (standardizes) a matrix.
+
+  Args:
+    X: A numpy.ndarray of shape (d, nx) to normalize.
+    m: A numpy.ndarray of shape (nx,) that contains the mean of all features of X.
+    s: A numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X.
+
+  Returns:
+    The normalized X matrix.   
+
+  """
+
+  # Normalize each feature by subtracting the mean and dividing by the standard deviation
+  X_normalized = (X - m) / s
+
+  return X_normalize
