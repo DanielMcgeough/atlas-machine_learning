f6ac688b4e9d57191b047aaa05f1fce936112a5a
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-09 16:47:29 -0500

    five sided fistagon?

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -2,14 +2,19 @@
 """shorter than I remember"""
 import numpy as np
 
+
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Updates the learning rate using inverse time decay in numpy.
 
     Args:
         alpha: The original learning rate.
-        decay_rate: The weight used to determine the rate at which alpha will decay.
-        global_step: The number of passes of gradient descent that have elapsed.
-        decay_step: The number of passes of gradient descent that should occur before alpha is decayed further.
+        decay_rate: The weight used to determine
+        the rate at which alpha will decay.
+        global_step: The number of passes of
+        gradient descent that have elapsed.
+        decay_step: The number of passes of
+        gradient descent that should occur
+        before alpha is decayed further.
 
     Returns: The updated value for alpha.
     """
