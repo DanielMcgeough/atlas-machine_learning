f87d3e326b337a01ca6ebc94a7bfe20793a47942
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-09 15:52:56 -0500

    indentation stuff.

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -2,23 +2,24 @@
 """This is needed to pass checker"""
 import numpy as np
 
+
 def normalization_constants(X):
-  """Calculates the normalization (standardization) constants of a matrix.
+    """Calculates the normalization (standardization) constants of a matrix.
 
-  Args:
-    X: A numpy.ndarray of shape (m, nx) to normalize.   
+    Args:
+        X: A numpy.ndarray of shape (m, nx) to normalize.   
 
-    m: The number of data points.
-    nx: The number of features.
+        m: The number of data points.
+        nx: The number of features.
 
-  Returns:
-    The mean and standard deviation of each feature, respectively.
+    Returns:
+        The mean and standard deviation of each feature, respectively.
   """
 
-  # Calculate the mean of each feature
-  mean = np.mean(X, axis=0)
+    # Calculate the mean of each feature
+    mean = np.mean(X, axis=0)
 
-  # Calculate the standard deviation of each feature
-  std_dev = np.std(X, axis=0)
+    # Calculate the standard deviation of each feature
+    std_dev = np.std(X, axis=0)
 
-  return mean, std_dev
+    return mean, std_dev
