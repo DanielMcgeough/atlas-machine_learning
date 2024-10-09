13dad874ed599ba0d14fe933bb4a574c38710a3d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-09 15:57:12 -0500

    whoops.

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -4,19 +4,19 @@ import numpy as np
 
 
 def normalize(X, m, s):
-  """Normalizes (standardizes) a matrix.
+    """Normalizes (standardizes) a matrix.
 
-  Args:
-    X: A numpy.ndarray of shape (d, nx) to normalize.
-    m: A numpy.ndarray of shape (nx,) that contains the mean of all features of X.
-    s: A numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X.
+    Args:
+        X: A numpy.ndarray of shape (d, nx) to normalize.
+        m: A numpy.ndarray of shape (nx,) that contains the mean of all features of X.
+        s: A numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X.
 
-  Returns:
-    The normalized X matrix.   
+    Returns:
+        The normalized X matrix.
 
   """
 
-  # Normalize each feature by subtracting the mean and dividing by the standard deviation
-  X_normalized = (X - m) / s
+    # Normalize each feature by subtracting the mean and dividing by the standard deviation
+    X_normalized = (X - m) / s
 
-  return X_normalize
+    return X_normalized
