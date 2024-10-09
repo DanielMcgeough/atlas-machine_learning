53165a01ad36a1d05377a780e9cbd70e245c0e75
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-09 16:08:38 -0500

    try this instead.

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -2,31 +2,8 @@
 """This is needed to pass checker"""
 import numpy as np
 
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
-def shuffle_data(X, Y):
-    """Shuffles the data points in two matrices the same way.
-
-    Args:
-        X: The first numpy.ndarray of shape (m, nx) to shuffle.
-        m: The number of data points.
-        nx: The number of features in X.
-        Y: The second numpy.ndarray of shape
-        (m, ny) to shuffle.
-        m: The same number of data points as in X.
-        ny: The number of features in Y.
-
-    Returns:
-        The shuffled X and Y matrices.
-  """
-
-    # Create a permutation of indices using numpy.random.permutation
-    permutation = np.random.permutation(X.shape[0])
-
-    # Shuffle X and Y using the same permutation
-    X_shuffled = X[permutation]
-    Y_shuffled = Y[permutation]
-
-    return X_shuffled, Y_shuffled
 
 def create_mini_batches(X, Y, batch_size):
     """Creates mini-batches to be used for training a neural network using mini-batch gradient descent.
