e0063dea41cc07c1f3e65b00d12b3c38c364bc22
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-12 12:17:21 -0500

    need to enumerate

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -4,14 +4,15 @@ import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    """Documentation"""
+    """Creating a confusion matrix with labels(actual)
+    and logits(predicted)"""
     true_labels = labels.argmax(axis=1)
     predicted_labels = logits.argmax(axis=1)
 
-    con_mat = np.zeros((len(np.unique(true_labels)
+    confused_matrix = np.zeros((len(np.unique(true_labels)
                             ), len(np.unique(predicted_labels))))
 
     for i, true_label in enumerate(true_labels):
-        con_mat[true_label, predicted_labels[i]] += 1
+        confused_matrix[true_label, predicted_labels[i]] += 1
 
-    return con_mat
+    return confused_matrix
