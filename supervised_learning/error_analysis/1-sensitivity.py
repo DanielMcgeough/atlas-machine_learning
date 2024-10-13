f8cf0bcd5dbdbb866851602ab4d5337787c2833f
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-13 14:45:22 -0500

    will that work?

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,13 +7,14 @@ def sensitivity(confusion):
     """Calculate the sensitivity
     otherwise known as the True
     Positive Rate."""
-    
+
     num_classes = confusion.shape[0]
     sensitivity_values = np.zeros(num_classes)
 
-    for i in range (num_classes):
+    for i in range(num_classes):
         true_positives = confusion[i, i]
         false_negatives = np.sum(confusion[i, :]) - true_positives
-        sensitivity_values[i] = true_positives / (true_positives + false_negatives)
+        sensitivity_values[i] = true_positives / \
+            (true_positives + false_negatives)
 
     return sensitivity_values
