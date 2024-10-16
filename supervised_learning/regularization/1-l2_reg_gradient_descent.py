15a6b385b6d8778b924f6d6846b9d69fc86e2252
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-15 22:34:18 -0500

    what thisa bout?

---
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Never know what to put here"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """Updates weights and biases of a neural network using gradient descent with L2 regularization.
+
+    Args:
+
+        Y: A one-hot numpy.ndarray of shape (classes, m) containing the correct labels.
+        weights: A dictionary of the weights and biases of the neural network.
+        cache: A dictionary of the outputs of each layer of the neural network.
+        alpha: The learning rate.
+        lambtha: The L2 regularization parameter.
+        L: The number of layers of the network.
+
+    """
+
+    m = Y.shape[1]
+
+    # Backward propagation
+    for l in reversed(range(L)):
+        if l == L - 1:
+            dA_prev = cache["A" + str(l + 1)] - Y
+    else:
+        dA_prev = np.dot(weights["W" + str(l + 1)].T, dA_prev) * (1 - np.square(cache["A" + str(l)]))
+
+    dW = (1 / m) * np.dot(dA_prev, cache["A" + str(l - 1)].T) + (lambtha / m) * weights["W" + str(l)]
+    db = (1 / m) * np.sum(dA_prev, axis=1, keepdims=True)
+
+    weights["W" + str(l)] -= alpha * dW
+    weights["b" + str(l)] -= alpha * db
