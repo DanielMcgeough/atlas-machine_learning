e64a67792d88004178669ca7e954b1fd97f52262
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-16 18:24:49 -0500

    ooof.

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -15,30 +15,58 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         A dictionary containing the outputs of each layer and the dropout mask used on each layer.
     """
 
+    # cache = {}
+    # D = {}
+
+    # # Input layer
+    # A0 = X
+    # cache["A0"] = A0
+
+    # # Hidden layers
+    # for l in range(1, L):
+    #     Z = np.dot(weights["W" + str(l)], A0) + weights["b" + str(l)]
+    #     A = np.tanh(Z)
+
+    #     # Dropout
+    #     D["D" + str(l)] = np.random.rand(*A.shape) < keep_prob
+    #     A *= D["D" + str(l)]
+    #     A /= keep_prob
+
+    #     cache["A" + str(l)] = A
+    #     A0 = A
+
+    # # Output layer
+    # ZL = np.dot(weights["W" + str(L)], A0) + weights["b" + str(L)]
+    # AL = np.exp(ZL) / np.sum(np.exp(ZL), axis=0)
+
+    # cache["AL"] = AL
+
+    # return cache, D
     cache = {}
-    D = {}
+    cache['A0'] = X
+    A = X
 
-    # Input layer
-    A0 = X
-    cache["A0"] = A0
+    for layer in range(1, L + 1):
+        W = weights[f'W{layer}']
+        b = weights[f'b{layer}']
 
-    # Hidden layers
-    for l in range(1, L):
-        Z = np.dot(weights["W" + str(l)], A0) + weights["b" + str(l)]
-        A = np.tanh(Z)
+        # Calculate current layer output
+        z = np.matmul(W, A) + b
 
-        # Dropout
-        D["D" + str(l)] = np.random.rand(*A.shape) < keep_prob
-        A *= D["D" + str(l)]
-        A /= keep_prob
+        if layer == L:
+            # Softmax activation for output layer
+            t = np.exp(z)
+            cache[f'A{layer}'] = t / np.sum(t, axis=0, keepdims=True)
+        else:
+            # tanh activation for hidden layers
+            A = np.tanh(z)
 
-        cache["A" + str(l)] = A
-        A0 = A
+            # Apply dropout
+            drop = np.random.rand(* A.shape) < keep_prob
+            A *= drop
+            A /= keep_prob
 
-    # Output layer
-    ZL = np.dot(weights["W" + str(L)], A0) + weights["b" + str(L)]
-    AL = np.exp(ZL) / np.sum(np.exp(ZL), axis=0)
+            cache[f'A{layer}'] = A
+            cache[f'D{layer}'] = drop.astype(int)
 
-    cache["AL"] = AL
-
-    return cache, D
+    return cache
