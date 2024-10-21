b1df77bc377c1810af4936062a2979a1c8eb6129
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 18:54:51 -0500

    Short but confusion.

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Keras yay"""
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """converts a vector into a
+    one-hot matrix"""
+    
+    return K.utils.to_categorical(labels, num_classes=classes)
