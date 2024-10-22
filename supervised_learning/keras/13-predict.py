49fede2e5878650c1b0b9295b396f690538e24d3
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 22:17:05 -0500

    was I smart or stupid.

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Keras yay"""
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """Makes a prediction using a neural
+    network.
+
+    Args:
+        network: The network model to make
+        the prediction with.
+        data: The input data to make the
+        prediction with.
+        verbose:
+        A boolean that determines if output
+        should be printed during the
+        prediction process.
+
+    Returns:
+        The prediction for the data.
+    """
+
+    return network.predict(data, verbose=verbose)
