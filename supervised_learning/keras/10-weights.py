d13020bd24fa805d69362354eba7845eb173b199
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 21:55:09 -0500

    spinning it up.

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Keras yay"""
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='keras'):
+    """Saves a model's weights.
+
+    Args:
+        network: The model whose weights should be saved.
+        filename: The path of the file that the weights should be saved to.
+        save_format:
+        The format in which the weights should be saved.
+    """
+
+    network.save_weights(filename, save_format=save_format)
+
+
+def load_weights(network, filename):
+        """Loads a model's weights.
+
+        Args:
+            network: The model to which the weights should be loaded.
+            filename: The path of the file that the weights should be loaded   
+            from.
+        """
+
+        network.load_weights(filename)
