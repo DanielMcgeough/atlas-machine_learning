44825a38af52a2d8b6d09dcca88e80ea645a2e74
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 22:09:31 -0500

    run it back.

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Keras yay"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Tests a neural network.
+
+    Args:
+        network: The network model to test.
+        data: The input data to test the model
+        with.
+        labels: The correct one-hot labels
+         of the data.
+        verbose: A boolean that determines if output should be printed during the testing process.
+
+    Returns:
+        The loss and accuracy of
+        the model with the testing data, respectively.
+    """
+
+    loss, accuracy = network.evaluate(data, labels, verbose=verbose)
+    return loss, accuracy
