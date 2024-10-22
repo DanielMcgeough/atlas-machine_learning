b1bafec86f5f87f0156a850f7063ef995e0e5a7d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 20:36:18 -0500

    added dox.

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -5,7 +5,10 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
-
+    """This is the same as 4 in that
+    it is training a model for mini batch
+    GD and adds a validation data
+    parameter to analyze."""
     network.compile(loss='categorical_crossentropy',
                     optimizer=network.optimizer, metrics=['accuracy'])
 
