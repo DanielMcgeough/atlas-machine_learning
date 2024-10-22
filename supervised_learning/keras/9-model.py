c9862caae4f7b1216c9729cfbcd091aa5f11219e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 21:50:26 -0500

    I hate eating a loss.

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Keras yay"""
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """does what it says"""
+    network.save(filename)
+
+def load_model(filename):
+    """loads an entire model"""
+    model = K.models.load_model(filename)
+    return model
