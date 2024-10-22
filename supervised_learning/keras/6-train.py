7fd2641232d2d8cd860364a743569df070b042be
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 20:44:04 -0500

    so you can remove compile becuase of vector creation.

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Keras yay"""
+
+import tensorflow.keras as K
+
+
+def train_model(network,
+                data,
+                labels,
+                batch_size,
+                epochs,
+                validation_data=None,
+                early_stopping=False,
+                patience=0,
+                verbose=True,
+                shuffle=False):
+    """This is the same as 4 in that
+    it is training a model for mini batch
+    GD and adds a validation data
+    parameter to analyze. Now we are
+    also adding an early stop functionality"""
+    
+    callbacks = []
+    if early_stopping and validation_data:
+        callbacks.append(K.callbacks.EarlyStopping(
+            monitor='val_loss', patience=patience
+        ))
+        
+    return network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        verbose=verbose,
+        shuffle=shuffle
+    )
