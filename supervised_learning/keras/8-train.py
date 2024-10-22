23bd28707422c12fe04e6e33731b471286241261
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 21:21:37 -0500

    ugh music today is stupid.

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -38,16 +38,16 @@ def train_model(network,
         def lr_schedule(epoch):
             return alpha / (1 + decay_rate * epoch)
 
-        callbacks.append(K.callbacks.LearningRateScheduler(lr_schedule,
-                                                           verbose=1))
+        callbacks.append(K.callbacks.LearningRateScheduler(
+            lr_schedule, verbose=1))
+
     if save_best and filepath and validation_data:
         callbacks.append(K.callbacks.ModelCheckpoint(
             filepath=filepath,
             save_best_only=True,
             monitor='val_loss',
             mode='min',
-            verbose=1
-        ))
+            verbose=1))
 
     K.backend.set_value(network.optimizer.learning_rate, alpha)
 
