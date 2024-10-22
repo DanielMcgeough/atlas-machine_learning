bd707e16ab4005f03c29fec9729c0f7e00885567
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-21 20:37:33 -0500

    pycode stuff

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -4,7 +4,14 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
+def train_model(network,
+                data,
+                labels,
+                batch_size,
+                epochs,
+                validation_data=None,
+                verbose=True,
+                shuffle=False):
     """This is the same as 4 in that
     it is training a model for mini batch
     GD and adds a validation data
