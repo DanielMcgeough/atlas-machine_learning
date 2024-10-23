f56a998e0255d7ba9cc9727a7079aa5edcc4cd78
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-23 18:10:21 -0500

    Ooof

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -8,23 +8,27 @@ def convolve_grayscale_valid(images, kernel):
 
     Args:
         images:
-    A numpy.ndarray with shape (m, h, w) containing multiple grayscale images.
-        kernel: A numpy.ndarray with shape (kh, kw) containing the kernel for the convolution.   
+    A numpy.ndarray with shape (m, h, w)
+    containing multiple grayscale images.
+        kernel: A numpy.ndarray with shape
+        (kh, kw) containing the kernel for
+        the convolution.
 
 
     Returns:
-        A numpy.ndarray containing the convolved images.
+        A numpy.ndarray containing the
+        convolved images.
     """
 
     m, h, w = images.shape
     kh, kw = kernel.shape
-
 
     output_shape = (m, h - kh + 1, w - kw + 1)
     output = np.zeros(output_shape)
 
     for i in range(output_shape[1]):
         for j in range(output_shape[2]):
-            output[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            output[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel,
+                                     axis=(1, 2))
 
     return output
