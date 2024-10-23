308032994399b176e0323c4b3a0c3b36c2919a06
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-23 18:33:49 -0500

    need to shore up understanding.

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -2,6 +2,7 @@
 """This is brutal"""
 import numpy as np
 
+
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """Performs a convolution on images
     with channels.
@@ -27,27 +28,26 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     kh, kw, _ = kernel.shape
     sh, sw = stride
 
-    # Calculate padding based on padding mode
+    assert c == kc, "Image channels and kernel channels must match"
+
     if padding == 'same':
-        ph = (h * sh - h + kh - 1) // 2
-        pw = (w * sw - w + kw - 1) // 2
+        ph = ((h - 1) * sh + kh - h) // 2 + (kh % 2 == 0)
+        pw = ((w - 1) * sw + kw - w) // 2 + (kw % 2 == 0)
     elif padding == 'valid':
-        ph = 0
-        pw = 0
+        ph, pw = 0, 0
     else:
         ph, pw = padding
 
-    # Pad the images
-    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                           mode='constant')
 
-    # Calculate the output shape
-    output_shape = (m, (h + 2*ph - kh + 1) // sh, (w + 2*pw - kw + 1) // sw, c)
-    output = np.zeros(output_shape)
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+    output = np.zeros((m, output_h, output_w))
 
-    # Perform the convolution
-    for i in range(0, output_shape[1], sh):
-        for j in range(0, output_shape[2], sw):
-            for k in range(c):
-                output[:, i, j, k] = np.sum(images[:, i:i+kh, j:j+kw, k] * kernel[:, :, k], axis=(1, 2))
+    for i in range(output_h):
+        for j in range(output_w):
+            roi = padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            output[:, i, j] = np.sum(roi * kernel, axis=(1, 2, 3))
 
     return output
