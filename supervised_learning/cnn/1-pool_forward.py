f22ef14bde27f14f87acd92a60d5242826da5f2a
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-30 18:16:29 -0500

    had to shorten it.

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -2,7 +2,10 @@
 """Cool but complex"""
 import numpy as np
 
-def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+def pool_forward(A_prev,
+                 kernel_shape,
+                 stride=(1, 1),
+                 mode='max'):
     """
     Performs forward propagation for a
     pooling layer.
