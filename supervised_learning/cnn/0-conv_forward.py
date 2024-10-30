a487ab7199b05dfeeef4e1948b070ed76170181f
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-10-30 18:10:07 -0500

    reworked a bit.

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -19,33 +19,35 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         Output activations of the current layer
     """
 
-    (m, h_prev, w_prev, c_prev) = A_prev.shape
-    (kh, kw, _, c_new) = W.shape
-    (sh, sw) = stride
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, c_new = W.shape
+    sh, sw = stride
 
-    # Calculate output dimensions
-    h_new = int(1 + (h_prev + 2 * padding - kh) / sh)
-    w_new = int(1 + (w_prev + 2 * padding - kw) / sw)
-
-    # Pad the input if 'same' padding
     if padding == 'same':
-        A_prev = np.pad(A_prev, ((0, 0), (kh//2, kh//2), (kw//2, kw//2), (0, 0)), 'constant')
+        ph = ((h_prev - 1) * sh + kh - h_prev + 1) // 2
+        pw = ((w_prev - 1) * sw + kw - w_prev + 1) // 2
 
-    # Initialize output array
-    Z = np.zeros((m, h_new, w_new, c_new))
+    elif padding == 'valid':
+        ph, pw = 0, 0
 
-    # Perform convolution
-    for h in range(h_new):
-        for w in range(w_new):
-            for c in range(c_new):
-                vert_start = h * sh
-                vert_end = vert_start + kh
-                horiz_start = w * sw
-                horiz_end = horiz_start + kw
+    else:
+        ph, pw = padding
 
-        Z[:, h, w, c] = np.sum(A_prev[:, vert_start:vert_end, horiz_start:horiz_end, :] * W[:, :, :, c], axis=(1, 2, 3)) + b[:, :, :, c]
+    A_prev_padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                           mode='constant')
 
-    # Apply activation function
+    output_h = (h_prev + 2 * ph - kh) // sh + 1
+    output_w = (w_prev + 2 * pw - kw) // sw + 1
+    Z = np.zeros((m, output_h, output_w, c_new))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            for k in range(c_new):
+                location = A_prev_padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+
+                Z[:, i, j, k] = np.sum(location * W[..., k],
+                                       axis=(1, 2, 3)) + b[..., k]
+
     A = activation(Z)
 
     return A
