a375e6deede5977b7df271c4306f8546247e60d3
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-06 17:04:43 -0600

    what?

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -21,36 +21,36 @@ def inception_block(A_prev, filters):
 
     # 1x1 Convolution Branch
     conv1_1x1 = K.layers.Conv2D(filters[0],
-                                (1,1),
+                                (1,1), 
                                 activation='relu',
                                 padding='same')(A_prev)
 
     # 3x3 Convolution Branch
     conv3_3x3 = K.layers.Conv2D(filters[1],
-                                (1,1),
+                                (1,1), 
                                 activation='relu',
                                 padding='same')(A_prev)
     conv3_3x3 = K.layers.Conv2D(filters[2],
-                                (3,3),
+                                (3,3), 
                                 activation='relu',
                                 padding='same')(conv3_3x3)
 
     # 5x5 Convolution Branch
     conv5_5x5 = K.layers.Conv2D(filters[3],
-                                (1,1),
+                                (1,1), 
                                 activation='relu',
                                 padding='same')(A_prev)
     conv5_5x5 = K.layers.Conv2D(filters[4],
-                                (5,5),
+                                (5,5), 
                                 activation='relu',
                                 padding='same')(conv5_5x5)
 
     # Pooling Branch
-    pool = K.layers.MaxPooling2D((3,3),
+    pool = K.layers.MaxPooling2D((3,3), 
                                  strides=(1,1),
                                  padding='same')(A_prev)
     pool = K.layers.Conv2D(filters[5],
-                           (1,1),
+                           (1,1), 
                            activation='relu',
                            padding='same')(pool)
 
