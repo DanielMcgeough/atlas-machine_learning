9189dd67f06361218a40c8d97815a52692531982
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-13 16:30:55 -0600

    This is all I have for now.

---
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Lots of stuff to setup"""
+import tensorflow as tf
+from tensorflow import keras as K
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical 
+
+
+def preprocess_data(X, Y):
+    # Normalize the image data because CIFAR-10
+    # has pictures that are 32 by 32 pixels so
+    # need to normalize them on 255 by 255 format
+    X = X.astype('float32') / 255.0
+
+    # One-hot encode the labels
+    Y = to_categorical(Y, 10)
+
+    return X, Y
+
+def train_cifar10_model():
+    # Load the CIFAR-10 dataset
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    # Preprocess the data
+    x_train, y_train = preprocess_data(x_train, y_train)
+    x_test, y_test = preprocess_data(x_test, y_test)
+
+    # Create the base model using VGG16 as a base is usually adequate
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+
+    # Freeze the base model this prevents the layers in the model from being adjusted
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # Add custom layers on top
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    predictions = Dense(10, activation='softmax')(x)
+
+    # Create the final model
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+
+    # Compile the model
+    model.compile(optimizer=Adam(learning_rate=0.0001),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Training the model with ten epochs
+    history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+
+    # Save the model
+    model.save('cifar10.h5')
+
+    # The script won't run when imported per the instructions
+    if __name__ == '__main__':
+        train_cifar10_model()
