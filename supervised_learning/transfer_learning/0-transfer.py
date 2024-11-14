35223c4c395663cb8cf0f046a36f43748ae4b7df
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-14 10:27:58 -0600

    closest I can get.

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -21,7 +21,7 @@ def preprocess_data(X, Y):
     # One-hot encode the labels
     Y = to_categorical(Y, 10)
     print(f"Error:31")
-    return (X, Y)
+    return(X, Y)
 
 def train_cifar10_model():
     print(f"Error: 1")
