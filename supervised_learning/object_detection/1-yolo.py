c96b4aaaf21c3e0275cc947e8c32e61a733c202d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-27 14:13:51 -0600

    as far as I got so far, establish new def

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -44,39 +44,3 @@ class Yolo:
 def process_outputs(self, outputs, image_size):
         boxes, box_confidences, box_class_probs = [], [], []
 
-        for output in outputs:
-            grid_h, grid_w, num_boxes = output.shape[:3]
-
-            # Reshape the output
-            output = output.reshape(grid_h, grid_w, num_boxes, 4 + 1 + len(self.class_names))
-
-            # Decode bounding box coordinates
-            box_xy, box_wh, box_confidence, box_class_probs = output[..., :2], output[..., 2:4], output[..., 4], output[..., 5:]
-
-            # Convert (x, y) to (x1, y1, x2, y2)
-            box_xy = (box_xy + np.meshgrid(np.arange(grid_h), np.arange(grid_w), indexing='ij')) / (grid_h, grid_w)
-            box_wh = np.exp(box_wh) * self.anchors[output_idx] / (416, 416)
-            box_xy -= box_wh / 2
-            box_xy *= image_size
-            box_wh *= image_size
-            boxes.append(np.concatenate([box_xy, box_xy + box_wh], axis=-1))
-
-            box_confidences.append(box_confidence)
-            box_class_probs.append(box_class_probs)
-
-        # Concatenate outputs from different layers
-        boxes = np.concatenate(boxes, axis=0)
-        box_confidences = np.concatenate(box_confidences, axis=0)
-        box_class_probs = np.concatenate(box_class_probs, axis=0)
-
-        # Apply non-maximum suppression
-        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-            boxes=tf.convert_to_tensor(boxes),
-            scores=tf.convert_to_tensor(box_confidences),
-            classes=tf.convert_to_tensor(np.argmax(box_class_probs, axis=-1)),
-            max_output_size=100,
-            iou_threshold=self.nms_t,
-            score_threshold=self.class_t
-        )
-
-        return boxes.numpy(), scores.numpy(), classes.numpy()
