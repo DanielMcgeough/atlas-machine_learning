7780bacad497b3c07eac51657cf0138968768671
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-27 13:55:50 -0600

    Complete rework to test checker

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
-"""This Object Detection Stuff is Wild."""
-
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-import numpy as np
-import keras as K
+""" Module defines the lass Yolo """
+from tensorflow import keras as K
 
 
-class YOLO:
-    """Initilaize YOLO v3 class and setup some other vars
-    Its concerning because it will get long after this."""
+class Yolo:
+    """ Class implements the Yolo v3 algorithm to perform object detection """
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        Class constructor
+
+        Arguments:
+            model_path (str): path to where Darknet Keras model is stored
+
+            classes_path (str): path to list of class names in order of index
+                used for the model
+
+            class_t (float): box score threshold to filter boxes by
+
+            nms_t (float): IOU threshold for non-max suppression
+
+            anchors (numpy.ndarray): array of shape (outputs, anchor_boxes, 2)
+                containing all anchor boxes:
+                    outputs: number of outputs (predictions) made
+                    anchor_boxes: number of anchor boxes for each prediction
+                    2: [anchor_box_width, anchor_box_height]
+
+        """
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = [line.strip() for line in f]
