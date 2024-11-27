4cc2edf78349c4882a861a80fe34909e54795e64
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-27 13:50:17 -0600

    setup of dox

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -7,6 +7,8 @@ import numpy as np
 
 
 class YOLO:
+    """Initilaize YOLO v3 class and setup some other vars
+    Its concerning because it will get long after this."""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = load_model(model_path)
         with open(classes_path, 'r') as f:
