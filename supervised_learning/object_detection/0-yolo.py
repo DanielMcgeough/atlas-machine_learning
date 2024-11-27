f33398dd5c9fa4b0279fc04a55ca1ef36fd542a0
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-27 13:52:49 -0600

    method shift

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -10,9 +10,9 @@ class YOLO:
     """Initilaize YOLO v3 class and setup some other vars
     Its concerning because it will get long after this."""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        self.model = load_model(model_path)
+        self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
-            self.class_names = f.read().splitlines()
+            self.class_names = [line.strip() for line in f]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
