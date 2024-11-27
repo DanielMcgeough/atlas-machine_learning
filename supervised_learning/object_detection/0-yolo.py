f28d01bcf79903221afe3d7d65c6e224ec2de933
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-11-27 13:49:17 -0600

    First setup of Yolo class

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+"""This Object Detection Stuff is Wild."""
 
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+
+
+class YOLO:
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = f.read().splitlines()
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
