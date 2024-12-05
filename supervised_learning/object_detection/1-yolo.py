04a62a1f945b78aa49766fd46384f49dff83eb5d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-12-04 19:43:53 -0600

    too may blank lines.

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -23,7 +23,6 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
-
     def process_outputs(self, outputs, image_size):
         """Processes outputs from
         Darknet model for one image"""
