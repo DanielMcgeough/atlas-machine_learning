f1a9c36ae3d21121d46322c5c167c3777f4cd2dd
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-01-14 21:31:31 -0600

    Lets see if we can put this to bed.

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -39,3 +39,32 @@ class MultiNormal:
         # (1/(n-1)) * centered_data @ centered_data.T
         self.cov = np.matmul(centered_data, centered_data.T) / (n - 1)
         # shape (d, d)
+
+    def pdf(self, x):
+        """calculates the PDF at a data"""
+
+        d = self.mean.shape[0]
+
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        if x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+
+        # get determinant and inverse
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+
+        # deviation from mean
+        diff = x - self.mean
+
+        # calculating quadratic term in exponent
+        exp = -0.5 * np.dot(np.dot(diff.T, inv), diff)
+
+        # Calculating normalizating constant
+        norm = 1 / np.sqrt((2 * np.pi) ** d * det)
+
+        # combining terms to get pdf value
+        pdf = float(norm * np.exp(exp))
+
+        return pdf
