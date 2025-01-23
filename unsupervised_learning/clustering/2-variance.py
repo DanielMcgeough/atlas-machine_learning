ea0e11cbcc7be8a5978f711af71e909240dfb208
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-01-22 18:36:48 -0600

    test point.

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""variance is an important
-value to consider for clusters
-especially when determining K"""
+"""module for variance"""
 import numpy as np
 
 
 def variance(X, C):
-    """Function that calculates the total
-    intra-cluster variance for a dataset
-        X is a numpy.ndarray of shape
-        (n, d) containing the data set
-        C is a numpy.ndarray of shape
-        (k, d) containing the centroid
-        means for
+    """Function that calculates the total intra-cluster variance for a dataset
+        X is a numpy.ndarray of shape (n, d) containing the data set
+        C is a numpy.ndarray of shape (k, d) containing the centroid means for
         each cluster
     Returns: var or None on failure
         var is the total variance"""
