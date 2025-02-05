62f7a8e3e455c4a5f03d271374cd05bdca21783e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-02-05 17:48:44 -0600

    Pylance fix.

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -5,22 +5,30 @@ import numpy as np
 
 def markov_chain(P, s, t=1):
     """
-    Determines the probability of a Markov chain being in a particular state 
+    Determines the probability of a Markov
+    chain being in a particular state 
     after a specified number of iterations.
-
     Args:
-        P: A square 2D numpy.ndarray of shape (n, n) representing the transition matrix.
-        P[i, j] is the probability of transitioning from state i to state j.
-        s: A numpy.ndarray of shape (1, n) representing the probability of starting 
+        P: A square 2D numpy.ndarray of shape
+        (n, n) representing the transition
+        matrix.
+        P[i, j] is the probability of
+        transitioning from state i to state j.
+        s: A numpy.ndarray of shape (1, n)
+        representing the probability of
+        starting 
         in each state.
-        t: The number of iterations that the markov chain has been through. 
+        t: The number of iterations that the
+        markov chain has been through. 
         Defaults to 1.
-
     Returns:
-        A numpy.ndarray of shape (1, n) representing the probability of being in 
-        a specific state after t iterations, or None on failure.
+        A numpy.ndarray of shape (1, n)
+        representing the probability of
+        being in 
+        a specific state after t iterations,
+        or None on failure.
     """
-    
+
     try:
         result = np.matmul(s, np.linalg.matrix_power(P, t))
         return result
