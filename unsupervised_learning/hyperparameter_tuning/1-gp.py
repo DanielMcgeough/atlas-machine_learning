d2a24fd1f71162266665dddf10c04a5898f4b295
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-02-13 11:33:16 -0600

    Math needed reworking.

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -4,16 +4,18 @@ import numpy as np
 
 
 class GaussianProcess:
-    """Class that represents a noiseless 1D Gaussian process"""
+    """Class that represents a noiseless
+    1D Gaussian process"""
+
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """a noiseless 1D Gaussian process:
-        X_init: numpy.ndarray of shape (t, 1)
-        representing the inputs already
+        X_init: numpy.ndarray of shape
+        (t, 1) representing the inputs already
         sampled with the black-box function
-        Y_init: numpy.ndarray of shape (t, 1)"
-        representing the outputs of the
-        black-box function for each input in
-        X_init
+        Y_init: numpy.ndarray of shape
+        (t, 1)" representing the outputs of the
+        black-box function for each input
+        in X_init
         t is the number of initial samples
         l is the length parameter for the
         kernel
@@ -34,16 +36,11 @@ class GaussianProcess:
         self.K = self.kernel(self.X, self.X)
 
     def kernel(self, X1, X2):
-        """calculates the covariance kernel
-        matrix between to matrices:
-            X1 is a numpy.ndarray of shape
-            (m, 1)
-            X2 is a numpy.ndarray of shape
-            (n, 1)
-            the kernel should use Radial Basis
-            Function(RBF)
-        Returns: the covariance kernel matrix
-        as a numpy.ndarray of
+        """calculates the covariance kernel matrix between to matrices:
+            X1 is a numpy.ndarray of shape (m, 1)
+            X2 is a numpy.ndarray of shape (n, 1)
+            the kernel should use Radial Basis Function(RBF)
+        Returns: the covariance kernel matrix as a numpy.ndarray of
         shape (m, n)"""
         X1_sq = np.sum(X1**2, 1).reshape(-1, 1)
         X2_sq = np.sum(X2**2, 1)
@@ -51,19 +48,24 @@ class GaussianProcess:
 
         sqdist = X1_sq + X2_sq - 2 * mult
 
-        self.K = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        K = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
 
-        return self.K
+        return K
 
     def predict(self, X_s):
-        """predicts the mean and stdv of points is a Gaussin process:
-        X_s is a numpy.ndarray of shape (s, 1) containing all of the points
-        whose mean and stdv should be calculated
+        """predicts the mean and stdv of
+        points is a Gaussin process:
+        X_s is a numpy.ndarray of shape
+        (s, 1) containing all of the points
+        whose mean and stdv should be
+        calculated
             s is the number of sample points
         Returns: mu, sigma
-            mu is a numpy.ndarray of shape (s,) containing the mean for each
+            mu is a numpy.ndarray of shape
+            (s,) containing the mean for each
             point in X_s, respectively
-            sigma is a numpy.ndarray of shape (s,) containing the variance for
+            sigma is a numpy.ndarray of shape
+            (s,) containing the variance for
             each point in X_s, respectively"""
         if not isinstance(X_s, np.ndarray):
             raise TypeError("X_s must be a numpy.ndarray")
