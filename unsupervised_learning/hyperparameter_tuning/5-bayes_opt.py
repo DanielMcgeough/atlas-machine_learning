a7ddb34be4d2eac9370e807c5bd6270b87d18b49
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-02-17 20:26:48 -0600

    Lot of rework

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -1,82 +1,116 @@
 #!/usr/bin/env python3
-"""Module for initialize bayesian optimization"""
+"""module for bayesian optimization"""
 import numpy as np
+from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """Class constructor def __init__(self, f, X_init, Y_init, bounds,
-    ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
-        f is the black-box function to be optimized
-        X_init is a numpy.ndarray of shape (t, 1) representing the inputs
-        already sampled with the black-box function
-        Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
-        of the black-box function for each input in X_init
-        t is the number of initial samples
-        bounds is a tuple of (min, max) representing the bounds of the space
-        in which to look for the optimal point
-        ac_samples is the number of samples that should be analyzed
-        during acquisition
-        l is the lnegth parameter for the kernel
-        sigma_f is the standard deviation given to the output of the black-box
-        function
-        xsi is the exploration-exploitation factor for acquisition
-        minimize is a bool determining whether optimization should be performed
-        for minimization(True) or maximization(False)
-        Sets the following public instance attributes:
-            f: the black-box function
-            gp: an instance of the class GaussianProcess
-            X_s: a numpy.ndarray of shape (ac_samples, 1) containing all
-            acquisition sample points, evenly spaced between min and max
-            xsi: the exploration-exploitation factor
-            minimize: a bool for minimization versus maximisation"""
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
-                 xsi=0.01, minimize=True):
+    """
+    Class that intializes and runs bayesian optimization
+    """
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """
+        Initialize Bayesian optimization.
+
+        Args:
+            f:          function to be optimized
+            X_init:     Initial X inputs matrix of shape (t, 1)
+            Y_init:     Initial Y outputs matrix of shape (t, 1)
+            bounds:     Tuple of (min, max) fur searching optimal point
+            ac_samples: Number of samples fur acquisition analysis
+            l:          Length param fur the kernel (default=1)
+            sigma_f:    Standard deviation fur black-box function (default=1)
+            xsi:        Exploration-exploitation factor (default=0.01)
+            minimize:   True fur minimization, False fur maximization
+                        (default=True)
+        """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
 
-        min, max = bounds
-        self.X_s = np.linspace(min, max, ac_samples).reshape(-1, 1)
+        # Generate acquisition sample points
+        min_bound, max_bound = bounds
+        self.X_s = np.linspace(min_bound,
+                               max_bound,
+                               ac_samples).reshape(-1, 1)
 
         self.xsi = xsi
         self.minimize = minimize
 
     def acquisition(self):
-        """Method that calculates the next best sample location:
-            Uses the Expected Improvement acquisition function
-                Returns: X_next, EI
-                    X_next is a numpy.ndarray of shape (1,) representing the
-                    next best sample point
-                    EI is a numpy.ndarray of shape (ac_samples,) containing the
-                    expected improvement of each potential sample"""
-        from scipy.stats import norm
+        """
+        Calculations fur next best sample location using Expected Improvement (EI).
 
-        posterior_mean, posterior_std = self.gp.predict(self.X_s)
+        Returns:
+            tuple: X_next (1,) array of next best point,
+                  EI (ac_samples,) array of expected improvements
+        """
+        mu, sigma = self.gp.predict(self.X_s)
 
+        # Get current best depending on minimization/maximization
         if self.minimize:
-            best_y = np.min(self.gp.Y)
+            mu_sample_opt = np.min(self.gp.Y)
         else:
-            best_y = np.max(self.gp.Y)
+            mu_sample_opt = np.max(self.gp.Y)
 
+        # Needed fur stability
+        sigma = np.maximum(sigma, 1e-9)
+
+        # Calculate improvement based on optimization type
         with np.errstate(divide='warn'):
             if self.minimize:
-                improve = best_y - posterior_mean - self.xsi
+                imp = mu_sample_opt - mu - self.xsi
             else:
-                improve = posterior_mean - best_y - self.xsi
+                imp = mu - mu_sample_opt - self.xsi
 
-        norm_imp = np.zeros_like(posterior_std)
-        valid_std_mask = posterior_std > 0
-        num = improve[valid_std_mask]
-        dem = posterior_std[valid_std_mask]
-        norm_imp[valid_std_mask] = num / dem
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
 
-        ll = num * norm.cdf(norm_imp[valid_std_mask])
-        rr = dem * norm.pdf(norm_imp[valid_std_mask])
-        expected_improvement = np.zeros_like(norm_imp)
-        expected_improvement[valid_std_mask] = ll + rr
+            # Set EI to 0 where sigma is 0
+            ei[sigma == 0] = 0
 
-        expected_improvement[posterior_std == 0.0] = 0
+        # Find index of best EI
+        X_next = self.X_s[np.argmax(ei)]
 
-        next_sample_point = self.X_s[np.argmax(expected_improvement)]
+        return X_next, ei
 
-        return next_sample_point, expected_improvement
+    def optimize(self, iterations=100):
+        """
+        Optimize that black-box function
+        The exact mathematical relationship between hyperparameters
+        and model performance is usually unknown.
+        It's a "black box" because:
+        We can't directly calculate the optimal hyperparameter values.
+        We don't have a formula to predict the performance for every possible combination.
+        We can only observe the output (model performance) for a given input (hyperparameter settings).
+
+        Args:
+            iterations: Max number of iterations 2 perform (default=100)
+
+        Returns:
+            tuple: X_opt (1,) optimal point array,
+                  Y_opt (1,) optimal value array
+        """
+        for _ in range(iterations):
+            X_next, _ = self.acquisition()
+
+            # Check if point was already sampled
+            if np.any(np.abs(X_next - self.gp.X) <= 1e-10):
+                break
+
+            # Sample new point and update GP
+            Y_next = self.f(X_next)
+            self.gp.update(X_next.reshape(-1, 1), Y_next.reshape(-1, 1))
+
+        # Get optimal point based on minimization/maximization
+        if self.minimize:
+            idx_opt = np.argmin(self.gp.Y)
+        else:
+            idx_opt = np.argmax(self.gp.Y)
+
+        X_opt = self.gp.X[idx_opt]
+        Y_opt = self.gp.Y[idx_opt]
+
+        return X_opt, Y_opt
