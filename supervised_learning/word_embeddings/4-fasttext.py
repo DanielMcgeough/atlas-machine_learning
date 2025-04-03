e9635e6192bebb671ff09776e367f9b0f1984b07
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-04-03 11:34:28 -0500

    reset to highest score.

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""module for fasttext"""
+"""module for fastext"""
 from gensim.models import FastText
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
-                    window=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Function that creates, builds and trains a gensim fastText model
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Function that creates, builds and trains a genism fastText model
         sentences: list of sentences to be trained on
         vector_size: dimensionality of the embedding layer
         min_count: min number of occurrences of a word for use in training
@@ -14,7 +14,7 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
         cbow: a boolean to determine the training type;
             True is for CBOW
             False is for Skip-gram
-        epochs: number of iterations to train over
+        epoch: number of iterations to train over
         seed: seed for the random number generator
         workers: number of worker threads to train the model
 
@@ -25,15 +25,13 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
         subword information. It can use either CBOW
         or Skip-gram architectures, like Word2Vec,
         but its character n-gram approach makes it
-        particularly effective for morphologically rich languages.
-    """
+        particularly effective for morphologically rich languages."""
     # Set training algorithm for cbow
     # Because it's CBOW sg = 0, otherwise it would be sg = 1
     sg = 0 if cbow else 1
 
     # fastText model with specific parameters
     model = FastText(
-        sentences=sentences, #Pass sentences during initialization.
         vector_size=vector_size,
         window=window,
         min_count=min_count,
@@ -42,6 +40,16 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
         seed=seed,
         workers=workers,
         epochs=epochs
+    )
+
+    # Build the vocab from sentences
+    model.build_vocab(sentences)
+
+    # Train model on the input sentences
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs
     )
 
     return model
