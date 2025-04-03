4d3aae1b5e063c7ac711c11d9295cd03bd7e461f
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-04-03 11:08:31 -0500

    switch back to get 3 points instead of 2

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """module for fastext"""
-# import tensorflow as tf
-# from gensim.models import FastText
-import gensim
+import tensorflow as tf
+from gensim.models import FastText
+
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
