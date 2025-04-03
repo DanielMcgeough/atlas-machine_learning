823cb31f6fad03f3407f658f6c20969538759072
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-04-03 11:19:57 -0500

    pycodestyle fixes

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -8,21 +8,21 @@ def gensim_to_keras(model):
     Returns: the trainable keras Embedding"""
     # .wv can have:
     # vectors: numpy array of shape (vacab size and vector size)
-        # with the word embeddings
+    # with the word embeddings
     # key_to_index: dict that maps words(as strings) to their correct indices
-        # in vector array
+    # in vector array
     # index_to_key: list that maps indices back to words, so you can see which
-        # word corresponds to which vector
-    
+    # word corresponds to which vector
+
     # Get vocab size and vector dimensions from model
-    
+
     # NumPy array that contains all word vectors
-    
-    ## Each row goes to a word in the vocab
-    ## Each column is a dimension of the embedding space
+
+    # Each row goes to a word in the vocab
+    # Each column is a dimension of the embedding space
     vocab = model.wv.vectors.shape[0]
-    
-    ## Shows how many numbers are used to represent each word
+
+    # Shows how many numbers are used to represent each word
     vector = model.wv.vectors.shape[1]
 
     # Extract word vectors from model
