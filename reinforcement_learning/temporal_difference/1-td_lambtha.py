bb6941c3d9b0b8a57561c6cd436a47d09b23d023
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-05-22 11:03:10 -0500

    updated pycodestyle.

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -3,7 +3,7 @@
 """
 Monte Carlo was the first task, it can be quick but TD updates in real time and
 although more expensive, seems to require less episodes. It is also the name
-of this project I mean come on! 
+of this project I mean come on!
 """
 
 import numpy as np
@@ -20,26 +20,36 @@ def td_lambtha(
     gamma=0.99
 ):
     """
-    Performs the TD(λ) algorithm for value estimation.
+    Performs the TD(λ) algorithm for value
+    estimation.
 
     Args:
-        env (object): The environment instance (expected to be a Gymnasium Env).
-        V (numpy.ndarray): A numpy.ndarray of shape (s,) containing the
+        env (object): The environment instance
+        (expected to be a Gymnasium Env).
+        V (numpy.ndarray): A numpy.ndarray of
+        shape (s,) containing the
             value estimate for each state.
-        policy (function): A function that takes in a state and returns
+        policy (function): A function that
+        takes in a state and returns
             the next action to take.
-        lambtha (float): The eligibility trace factor.
-        episodes (int, optional): The total number of episodes to train over.
+        lambtha (float): The eligibility
+        trace factor.
+        episodes (int, optional): The total
+        number of episodes to train over.
             Defaults to 5000.
-        max_steps (int, optional): The maximum number of steps per episode.
+        max_steps (int, optional): The maximum
+        number of steps per episode.
             Defaults to 100.
-        alpha (float, optional): The learning rate. Defaults to 0.1.
-        gamma (float, optional): The discount rate. Defaults to 0.99.
+        alpha (float, optional): The learning
+        rate. Defaults to 0.1.
+        gamma (float, optional): The discount
+        rate. Defaults to 0.99.
 
     Returns:
-        numpy.ndarray: V, the updated value estimate.
+        numpy.ndarray: V, the updated value
+        estimate.
     """
-    
+
     for episode in range(episodes):
         state, _ = env.reset()
         e_traces = np.zeros_like(V)
