d04b67861980d55e1f72df83357f00c25e36ebef
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-05-27 23:27:02 -0500

    this probably will fail.

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -1,73 +1,52 @@
 #!/usr/bin/env python3
-"""documentation"""
+
+"""This is where we're going to train table
+so that it becomes less stupid in a more optimal set up.
+Honestly I'm thinking of just following this tutorial
+and saying F it? I'm not sure how I'll impliment the policy gradient.
+https://www.janisklaise.com/post/rl-policy-gradients/
+That tutorial proved to be relatively useless.
+https://www.youtube.com/watch?v=5eSh5F8gjWU
+This one seems to be more useful.
+Oh???? This stack overflow looks promising
+https://stackoverflow.com/questions/46597809/policy-gradient-methods-for-open-ai-gym-cartpole
+Nevermind ignore that one
+https://github.com/drozzy/reinforce/blob/main/reinforce.py
+Ok here's that youtuber's github.
+
+I'll be honest Sajid, I circled around this task quite a bit
+I have a vague understanding of the concepts, but I think I have to take the L on this task
+You can see a previous attempt under "dead.py"
+These projects were pretty dense, I regret procrastinating
+But I think I did fairly well all things considered.
+"""
+
 import numpy as np
+
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
 def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
-    """documentation"""
-    initial_state = env.reset()
-    if isinstance(initial_state, tuple):
-        state = initial_state[0]
-    else:
-        state = initial_state
+    """I'm *pretty* sure that we're not training a full model here
+    Just filling out the Q table
+    Unless it's not the Q table anymore?
+    env - initial environment
+    nb_episodes - the number of episodes
+    alpha - learning rate
+    gamma - discount factor"""
 
-    n_states = len(state)
-    n_actions = env.action_space.n
+    # Initializing weights
+    # Or trying to rather
+    weights = np.random.rand(4)
+    episode_rewards = []
 
-    weights = np.random.normal(size=(n_states, n_actions))
-
-    all_scores = []
-
-    for episode in range(nb_episodes):
-        initial_state = env.reset()
-        if isinstance(initial_state, tuple):
-            state = initial_state[0]
-        else:
-            state = initial_state
-
-        episode_rewards = []
-        episode_gradients = []
-
+    for i in range(nb_episodes):
         done = False
-        while not done:
 
-            action, gradient = policy_gradient(state, weights)
+        actions = []
+        states = []
+        rewards = []
 
-            step_result = env.step(action)
+        # while not done:
 
-            if len(step_result) == 4:
-                next_state, reward, done, _ = step_result
-            else:
-                next_state, reward, terminated, truncated, _ = step_result
-                done = terminated or truncated
-
-            if isinstance(next_state, tuple):
-                next_state = next_state[0]
-
-            episode_rewards.append(reward)
-            episode_gradients.append(gradient)
-
-            state = next_state
-
-        discounted_rewards = []
-        cumulative_reward = 0
-        for reward in reversed(episode_rewards):
-            cumulative_reward = reward + gamma * cumulative_reward
-            discounted_rewards.insert(0, cumulative_reward)
-
-        discounted_rewards = np.array(discounted_rewards)
-        discounted_rewards = (discounted_rewards - np.mean
-                              (discounted_rewards)) / \
-            (np.std(discounted_rewards) + 1e-10)
-
-        for gradient, discounted_reward in zip(episode_gradients,
-                                               discounted_rewards):
-            weights += alpha * gradient * discounted_reward
-
-        episode_score = sum(episode_rewards)
-        all_scores.append(episode_score)
-
-        print("Episode: {} Score: {}".format(episode, episode_score))
-
-    return all_scores
+    return
