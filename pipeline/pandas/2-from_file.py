2213331741c86fffef95f208befed00b3df5091f
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-06-20 09:51:02 -0500

    pycodestsyle problems for whitespace

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -9,7 +9,7 @@ import pandas as pd
 def from_file(filename, delimiter):
     """Pulls a file and makes a dataframe
     of it"""
-    
+
     df_return = pd.read_csv(filename, delimiter=delimiter)
 
     return df_return
