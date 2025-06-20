93d09eb5afb9af1a08caea8c589e03788441949e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-06-20 13:18:53 -0500

    cool.

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
-"""This is brutal, how was I supposed
-to have any idea how to do this."""
+
+"""This uses the github api
+and is causing me psychosis."""
+
+import sys
 import requests
-from sys import argv
-import time
+from datetime import datetime
+
+
+def get_user_location(url):
+    """finds a user"""
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        return user_data.get("location", "Location not provided")
+    elif response.status_code == 404:
+        return "Not found"
+    elif response.status_code == 403:
+        reset_timestamp = int(response.headers.get("X-Ratelimit-Reset", 0))
+
+        reset_time = datetime.fromtimestamp(reset_timestamp)
+        current_time = datetime.now()
+        minutes_until_reset = (reset_time - current_time).seconds // 60
+        return f"Reset in {minutes_until_reset} min"
+    else:
+        return f"Unexpected status code: {response.status_code}"
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(argv[1])
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <GitHub API URL>")
+        sys.exit(1)
 
-        # if user not found
-        if r.status_code == 404:
-            print('Not found')
-
-        # print remaining time if rate limit exceeded
-        if r.status_code == 403:
-            now = int(time.time())
-            reset = int(r.headers['X-RateLimit-Reset'])
-            diff = reset - now
-            print("Reset in {} min".format(diff // 60))
-
-        # print location if no error
-        if r.status_code == 200:
-            print(r.json()['location'])
-
-    # Print exception if incorrect URL is passed
-    except requests.exceptions.MissingSchema as ms:
-        print(ms)
+    url = sys.argv[1]
+    location = get_user_location(url)
+    print(location)
